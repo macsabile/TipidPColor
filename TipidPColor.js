002960abb2fca93e0e95484bdacf8435ad953210
Mac Sabile <macsabile@gmail.com>
2016-02-24 13:39:13 +0800

    Update TipidPColor.js

--- TipidPColor.js
+++ TipidPColor.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
     checkSavedSettings();
  
     var ver = '1.3.1';
-    var build = '022415-2';
+    var build = '022416-2';
     var page = String(location).split('/')[3].split('.php')[0];
     var siteLogoValue = GM_getValue('HideSiteLogo');
     var forumPostValue = GM_getValue('forumPostFontSize');
